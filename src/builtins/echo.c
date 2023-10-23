6751f4031c6faaec8eecf0a5171da5fbd1052626
cwenz <wenzconrad@gmail.com>
2023-10-23 13:31:20 +0200

    updated echo cmd

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,21 +6,22 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 09:56:50 by cwenz             #+#    #+#             */
-/*   Updated: 2023/10/17 12:45:39 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/10/23 13:29:30 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	echo(char *str)
+
+// TODO: Pass down command table to it and store exit code:
+void	echo(void)
 {
-	char	*end;
-	
-	str += 8;
-	if (*str == '"')
-		str++;
-	end = str + ft_strlen(str) - 2;
-	if ((*end == '\'' || *end == '"'))
-		*end = '\0';
-	printf("%s", str);
+	bool has_n_flag = true; // placeholder for now, change this.
+	bool no_string = true; // placeholder for now, change this.
+	if (no_string)
+		printf("\n");
+	else if (has_n_flag)
+		printf("PRINT STRING HERE");
+	else
+		printf("PRINT STRING HERE\n");
 }

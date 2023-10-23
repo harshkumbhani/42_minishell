1161e11b1c0a2b33295d5127f7f7538d32527103
cwenz <wenzconrad@gmail.com>
2023-10-23 13:42:04 +0200

    wip

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,12 +6,11 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 09:56:50 by cwenz             #+#    #+#             */
-/*   Updated: 2023/10/23 13:29:30 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/10/23 13:36:47 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
 
 // TODO: Pass down command table to it and store exit code:
 void	echo(void)

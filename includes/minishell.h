b5cc13691a36ab6a6c9c54a2f21a88375c64b13c
cwenz <wenzconrad@gmail.com>
2023-10-27 09:31:20 +0200

    added struct

--- includes/minishell.h
+++ includes/minishell.h
@@ -40,6 +40,18 @@ typedef struct s_env {
 	struct s_env	*next;
 }	t_env;
 
+typedef struct s_minishell {
+	char	**cmd;
+	char	*infile;
+	t_env	*head_env;
+	char	*deli;
+	char	*outfile;
+	int		infile_fd;
+	int		outfile_fd;
+	bool	here_doc;
+	int		exit_code;
+}	t_minishell;
+
 int	g_signal_received;
 
 /* --------------------------------- Lexer -------------------------------- */

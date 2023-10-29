e8e6ee502e6a25c17e87d3af565dd77e7eae53ad
cwenz <wenzconrad@gmail.com>
2023-10-29 12:11:39 +0100

    added function to find command

---
+++ src/executor/command.c
@@ -0,0 +1,56 @@
+
+#include "minishell.h"
+
+static char	*find_cmd_path(t_cmd *cmd, char **envp);
+
+void	execute_cmd(t_cmd *cmds, t_env *head_env)
+{
+	t_env	*path_node;
+	char	*path;
+	char	**envp;
+
+	// TODO: PROTECT IF NULL??
+	path_node = find_env_key(head_env, "PATH");
+	envp = ft_split(path_node->value, ':');
+	path = find_cmd_path(cmds, envp);
+	if (!path)
+	{
+		// TODO: FREE STUFF??
+		printf("Unable to find path.\n");
+		exit(EXIT_FAILURE);
+	}
+	if (execve(path, cmds->cmd, envp) == EXIT_FAILURE)
+	{
+		free(path);
+		free_env_array(envp);
+		exit(EXIT_FAILURE);
+	}
+}
+
+static char	*find_cmd_path(t_cmd *cmd, char **envp)
+{
+	char	*path;
+	char	*temp;
+	int		i;
+	
+	path = strjoin_pipex(cmd->cmd[0], "");
+	if (access(path, F_OK | X_OK) == 0)
+		return (path);
+	free(path);
+	i = 0;
+	while (envp[i])
+	{
+		temp = strjoin_pipex(envp[i], "/");
+		if (!temp)
+			return (NULL);
+		path = strjoin_pipex(temp, cmd->cmd[0]);
+		if (!path)
+			return (NULL);
+		free(temp);
+		if (access(path, F_OK | X_OK) == 0)
+			return (path);
+		free(path);
+		i++;
+	}
+	return (NULL);
+}

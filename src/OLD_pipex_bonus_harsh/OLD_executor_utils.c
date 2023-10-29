baa68f707ea205ee639d0a07d8a932c7a6ba6e10
cwenz <wenzconrad@gmail.com>
2023-10-29 12:11:15 +0100

    moved pipex code into different folder (will delete it)

---
+++ src/OLD_pipex_bonus_harsh/OLD_executor_utils.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   OLD_executor_utils.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/16 06:32:12 by hkumbhan          #+#    #+#             */
+/*   Updated: 2023/10/27 16:59:09 by cwenz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex_bonus.h"
+
+void	close_fds_bonus(t_pip_bonus *pipex)
+{
+	if (pipex->infile_fd != -1)
+		close(pipex->infile_fd);
+	if (pipex->outfile_fd != -1)
+		close(pipex->outfile_fd);
+	if (pipex->fd[0] != -1)
+		close(pipex->fd[0]);
+	if (pipex->fd[1] != -1)
+		close(pipex->fd[1]);
+	if (pipex->hdfd[0] != -1)
+		close(pipex->hdfd[0]);
+	if (pipex->hdfd[1] != -1)
+		close(pipex->hdfd[1]);
+}
+
+void	free_bonus(t_pip_bonus *pipex)
+{
+	close_fds_bonus(pipex);
+	if (pipex->envp_path != NULL)
+		free_arr(pipex->envp_path);
+	pipex->envp_path = NULL;
+	if (pipex->tmp != NULL)
+		free(pipex->tmp);
+	pipex->tmp = NULL;
+	free(pipex);
+}
+
+int	open_file(char *file, int i)
+{
+	int	fd;
+
+	fd = 0;
+	if (i == 0)
+		fd = open(file, O_RDONLY);
+	else if (i == 1)
+		fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	else if (i == 2)
+		fd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	return (fd);
+}
+
+char	*strjoin_pipex(char *s1, char *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = -1;
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	str = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	while (s1 != NULL && s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2 != NULL && s2[++j] != '\0')
+		str[i + j] = s2[j];
+	return (str);
+}
---
+++ src/OLD_pipex_bonus_harsh/OLD_init.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   OLD_init.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/12 10:34:59 by hkumbhan          #+#    #+#             */
+/*   Updated: 2023/10/27 16:59:24 by cwenz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex_bonus.h"
+
+void	init(t_pip_bonus *pipex, int ac, char **av, char **ep)
+{
+	pipex->argc = ac;
+	pipex->argv = av;
+	pipex->envp = ep;
+	pipex->fd[0] = -1;
+	pipex->fd[1] = -1;
+	pipex->hdfd[0] = -1;
+	pipex->hdfd[1] = -1;
+	pipex->infile_fd = -1;
+	pipex->outfile_fd = -1;
+	pipex->tmp = NULL;
+	pipex->envp_path = NULL;
+	pipex->here_doc_flag = FALSE;
+	get_envp_path(pipex);
+	if (ft_strncmp(av[1], "here_doc", 8) == 0)
+		pipex->here_doc_flag = TRUE;
+}
+
+void	get_envp_path(t_pip_bonus *pipex)
+{
+	int	i;
+
+	i = -1;
+	while (pipex->envp[++i] != NULL)
+	{
+		if (ft_strncmp(pipex->envp[i], "PATH", 4) == 0)
+		{
+			ft_strlcat(pipex->path, pipex->envp[i] + 5,
+				ft_strlen(pipex->envp[i]) + 1);
+			break ;
+		}
+	}
+	pipex->envp_path = ft_split(pipex->path, ':');
+	if (pipex->envp_path == NULL)
+		handle_error_bonus(ERR_MEMORY, pipex);
+}

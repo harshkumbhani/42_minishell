da0009541e13e10d08c8d09086b0233b24eccd32
cwenz <wenzconrad@gmail.com>
2023-10-29 12:12:18 +0100

    removed comment

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -23,30 +23,4 @@ static void	execute_pipes(t_minishell *minishell)
 			execute_last_child(minishell, i);
 		i++;
 	}
-		/*
-		pipe(minishell->cmds[i]->fd);
-		pid = fork();
-
-		if (pid == -1)
-			exit(EXIT_FAILURE);
-		if (pid == 0)
-		{
-			close(minishell->cmds[i]->fd[0]);
-			dup2(minishell->cmds[i]->infile_fd, STDIN_FILENO);
-			dup2(minishell->cmds[i]->fd[1], STDOUT_FILENO);
-			close(minishell->cmds[i]->fd[1]);
-			execute_cmd(minishell->cmds[i], minishell->head_env);
-		}
-		else
-		{
-			close(minishell->cmds[i]->fd[1]);
-			if (i > 0)
-				close(minishell->cmds[i]->infile_fd);
-			minishell->cmds[i]->infile_fd = minishell->cmds[i]->fd[0];
-			close(minishell->cmds[i]->fd[0]);
-			waitpid(pid, &status, 0);
-			if (WIFEXITED(status))
-				minishell->exit_code = status;
-		}
-		*/
 }

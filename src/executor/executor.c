0c741202cdd993c7142bdea4040f5df25e3a5615
cwenz <wenzconrad@gmail.com>
2023-10-31 09:48:19 +0100

    changed variable name

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -4,7 +4,7 @@ static void	execute_pipes(t_minishell *minishell);
 
 void	executor(t_minishell *minishell)
 {
-	if (!minishell->cmds[1] && is_cmd_builtin(minishell))
+	if (!minishell->cmd_table[1] && is_cmd_builtin(minishell))
 		exec_builtins(minishell);
 	else
 		execute_pipes(minishell);
@@ -15,9 +15,9 @@ static void	execute_pipes(t_minishell *minishell)
 	int	i;
 
 	i = 0;
-	while (minishell->cmds[i])
+	while (minishell->cmd_table[i])
 	{
-		if (minishell->cmds[i + 1])
+		if (minishell->cmd_table[i + 1])
 			execute_child(minishell, i);
 		else
 			execute_last_child(minishell, i);
--- src/executor/handle_pipe.c
+++ src/executor/handle_pipe.c
@@ -8,25 +8,25 @@ void	execute_child(t_minishell *minishell, int index)
 	int	pid;
 	int	status;
 
-	pipe(minishell->cmds[index]->fd);
+	pipe(minishell->cmd_table[index]->fd);
 	pid = fork();
 	// if (pid == -1)
 	// 	// TODO: What if it fails?
 	if (pid == 0)
 	{
 		// if (index == 0) // first child
-		// 	open_infile(minishell->cmds[index]);
+		// 	open_infile(minishell->cmd_table[index]);
 
-		close(minishell->cmds[index]->fd[0]); // close read end of pipe
-		dup2(minishell->cmds[index]->fd[1], STDOUT_FILENO); // duplicate write end of pipe to STDIN_FILENO
-		close(minishell->cmds[index]->fd[1]); // close write end of pipe
-		execute_cmd(minishell->cmds[index], minishell->head_env);
+		close(minishell->cmd_table[index]->fd[0]); // close read end of pipe
+		dup2(minishell->cmd_table[index]->fd[1], STDOUT_FILENO); // duplicate write end of pipe to STDIN_FILENO
+		close(minishell->cmd_table[index]->fd[1]); // close write end of pipe
+		execute_cmd(minishell->cmd_table[index], minishell->head_env);
 	}
 	else
 	{
-		close(minishell->cmds[index]->fd[1]); // close write end of pipe
-		dup2(minishell->cmds[index]->fd[0], STDIN_FILENO); // duplicate write end of pipe to STDIN_FILENO
-		close(minishell->cmds[index]->fd[0]); // close read end of pipe
+		close(minishell->cmd_table[index]->fd[1]); // close write end of pipe
+		dup2(minishell->cmd_table[index]->fd[0], STDIN_FILENO); // duplicate write end of pipe to STDIN_FILENO
+		close(minishell->cmd_table[index]->fd[0]); // close read end of pipe
 		waitpid(pid, &status, 0);
 		if (WIFEXITED(status)) // get the exit status
 			minishell->exit_code = status;
@@ -44,11 +44,11 @@ void	execute_last_child(t_minishell *minishell, int index)
 	// 	// TODO: ???
 	if (pid == 0)
 	{
-		close(minishell->cmds[index]->fd[1]); // close write end of pipe
-		dup2(minishell->cmds[index]->fd[0], STDIN_FILENO); // duplicate write end of pipe to STDIN_FILENO
-		dup2(minishell->cmds[index]->outfile_fd, STDOUT_FILENO); // duplicate to std output
-		close(minishell->cmds[index]->fd[0]); // close read end of pipe
-		execute_cmd(minishell->cmds[index], minishell->head_env);
+		close(minishell->cmd_table[index]->fd[1]); // close write end of pipe
+		dup2(minishell->cmd_table[index]->fd[0], STDIN_FILENO); // duplicate write end of pipe to STDIN_FILENO
+		dup2(minishell->cmd_table[index]->outfile_fd, STDOUT_FILENO); // duplicate to std output
+		close(minishell->cmd_table[index]->fd[0]); // close read end of pipe
+		execute_cmd(minishell->cmd_table[index], minishell->head_env);
 	}
 	else
 	{

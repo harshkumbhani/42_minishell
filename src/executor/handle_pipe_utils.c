363de951093aa293ba1ab45631494635eb0de5cc
cwenz <wenzconrad@gmail.com>
2023-11-01 14:35:57 +0100

    fixed exit code when signal

--- src/executor/handle_pipe_utils.c
+++ src/executor/handle_pipe_utils.c
@@ -8,4 +8,7 @@ void	get_exit_status(t_minishell *minishell, int	pid)
 	waitpid(pid, &status, 0);
 	if (WIFEXITED(status))
 		minishell->exit_code = WEXITSTATUS(status);
+	// convention in unix operating systems to return the exit status as 128 + status when process is terminated by a signal
+	if (WIFSIGNALED(status))
+		minishell->exit_code = 128 + WTERMSIG(status);
 }

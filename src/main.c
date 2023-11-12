01bdbec3f5a7f4b60c9b4bc0a900fdaf7e553f5e
cwenz <wenzconrad@gmail.com>
2023-11-12 16:06:29 +0100

    cleaned up main function

--- src/main.c
+++ src/main.c
@@ -1,55 +1,49 @@
+
 #include "minishell.h"
 
-// TODO: Implement SHLVL
+static void	run_minishell(t_minishell *minishell);
 
 int main(int argc, char **argv, char **envp)
 {
 	(void)argc;
 	(void)argv;
 	t_minishell	minishell;
-	char		*input;
-	t_lexer		*lexer;
 
 	minishell = (t_minishell){};
 	copy_std_fds(&minishell);
 	export(&minishell.head_env, envp);
 	setup_signals();
+	run_minishell(&minishell);
+	free_env_linked_list(minishell.head_env);
+	return (0);
+}
+
+static void	run_minishell(t_minishell *minishell)
+{
+	char		*input;
+	t_lexer		*lexer;
+
 	while (1)
 	{
 		input = readline("minishell> ");
-		// if (!input)
-		// {
-		// 	printf("Ctrl+D was pressed!\n");
-		// 	lst_del(&lexer);
-		// 	free(input);
-		// 	break;
-		// }
-		lexer = tokenise(input);
-		//print_list(&lexer);
+		if (!input)
+		{
+			printf("Ctrl+D was pressed!\n");
+			lst_del(&lexer);
+			break ;
+		}
 		if (input && input[0] != '\0' && input[0] != '\n')
 			add_history(input);
-		if (parser(&lexer, &minishell) == FAIL)
+		lexer = tokenise(input);
+		if (parser(&lexer, minishell) == FAIL)
 		{
 			lst_del(&lexer);
-			continue;
+			continue ;
 		}
-		// print_cmd_table(&minishell.cmd_table[0]);
-		executor(&minishell);
-		free_cmd_table(minishell.cmd_table);
-		lst_del(&lexer);
+		executor(minishell);
 		free(input);
-		reset_fds(&minishell);
-		// dup2(stdin, STDIN_FILENO);
+		lst_del(&lexer);
+		free_cmd_table(minishell->cmd_table);
+		reset_fds(minishell);
 	}
-				
-	// lexer = tokenise("ls -al");
-	// if (lexer != NULL)
-	// {
-	// 	print_list(&lexer);
-	// }
-	// parser(&lexer, &minishell);
-	// executor(&minishell);
-	//lst_del(&lexer);
-	free_env_linked_list(minishell.head_env);
-	return (0);
 }

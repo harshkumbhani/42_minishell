4fd36160658b1a00e42f7a48cfed873e7b2c3863
cwenz <wenzconrad@gmail.com>
2023-11-13 22:50:42 +0100

    fixed exit builtin function

--- includes/builtin_prototypes.h
+++ includes/builtin_prototypes.h
@@ -6,7 +6,7 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 14:54:06 by cwenz             #+#    #+#             */
-/*   Updated: 2023/11/13 21:49:28 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/11/13 22:14:19 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int		env(t_env *head);
 int		echo(char **cmd);
 int		export(t_env **head, char **str);
 int		unset(t_env **head, char **key);
-void	builtin_exit(t_minishell *minishell);
+void	builtin_exit(t_minishell *minishell, int index);
 void	exec_builtins(t_minishell *minishell, int i);
 bool	is_cmd_builtin(t_minishell *minishell, int i);
 
--- src/builtins/exec_builtins.c
+++ src/builtins/exec_builtins.c
@@ -6,7 +6,7 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 16:38:01 by cwenz             #+#    #+#             */
-/*   Updated: 2023/11/13 22:01:54 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/11/13 22:18:48 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	exec_builtins(t_minishell *minishell, int i)
 	else if (ft_strcmp(minishell->cmd_table[i]->cmd[0], "echo") == 0)
 		minishell->exit_code = echo(minishell->cmd_table[i]->cmd);
 	else if (ft_strcmp(minishell->cmd_table[i]->cmd[0], "exit") == 0)
-		builtin_exit(minishell);
+		builtin_exit(minishell, i);
 	else if (ft_strcmp(minishell->cmd_table[i]->cmd[0], "export") == 0)
 		minishell->exit_code = export(&minishell->head_env,
 				minishell->cmd_table[i]->cmd);

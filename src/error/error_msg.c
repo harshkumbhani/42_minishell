b53ecd17b47bc8d492c0262645aa51a40c86ca1a
cwenz <wenzconrad@gmail.com>
2023-11-20 15:22:59 +0100

    modified error messages

--- src/error/error_msg.c
+++ src/error/error_msg.c
@@ -6,7 +6,7 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 16:51:45 by cwenz             #+#    #+#             */
-/*   Updated: 2023/11/13 21:30:09 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/11/20 14:07:53 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	error_msg(char *cmd, char *cmd2, char *reason)
 {
 	if (cmd && reason && cmd2)
-		ft_fprintf(STDERR_FILENO, "minishell: %s: %s: %s\n", cmd, cmd2, reason);
+		ft_fprintf(STDERR_FILENO, "\033[0;31m(!)\033[0m minishell: %s: %s: %s\n", cmd, cmd2, reason);
 	else if (cmd && reason)
-		ft_fprintf(STDERR_FILENO, "minishell: %s: %s\n", cmd, reason);
+		ft_fprintf(STDERR_FILENO, "\033[0;31m(!)\033[0m minishell: %s: %s\n", cmd, reason);
 }
